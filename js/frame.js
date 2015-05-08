16fe25f5c620c56eefffea662b514078d27227d7
xccui <xingcanc@gmail.com>
2015-05-08 21:04:13 +0800

    Complete the rest pages and add page anchors

--- js/frame.js
+++ js/frame.js
@@ -3,3 +3,13 @@ window.onload = function(){
 	var height = document.body.scrollHeight;	
 	parent.document.getElementsByName("main_frame")[0].style.height = (height + "px");
 }
+fixTop = 300;
+function setWindowScrollTop(p, id) {
+	var topHeight = document.getElementById(id).offsetTop;
+	if(p.document.documentElement) {
+		p.document.documentElement.scrollTop = topHeight + fixTop;
+	}
+	if(p.document.body) {
+		p.document.body.scrollTop = topHeight + fixTop;
+	}
+}
